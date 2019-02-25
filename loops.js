86e9b106d76051df8ed73e4b378de69fda7ace15
Shawn Wilson <shawn.wilson137@gmail.com>
2019-02-25 22:02:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,6 +12,7 @@ function forLoop(array) {
 function whileLoop() {
   let countdown = n;
   while (countdown > 0){
-      console.log(countdown--);
+    console.log(countdown--);
   }
+  return "done"
 }
